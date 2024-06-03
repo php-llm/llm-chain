22d4904c98477024777f9fab718edc2ad194371d
Christopher Hertel <mail@christopher-hertel.de>
2024-06-03 21:48:11 +0200

    fix: remove var_dump error handling from http client

--- src/OpenAI/OpenAIClient.php
+++ src/OpenAI/OpenAIClient.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace PhpLlm\LlmChain\OpenAI;
 
-use Symfony\Component\HttpClient\Exception\ClientException;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 final class OpenAIClient
@@ -24,19 +23,12 @@ final class OpenAIClient
     {
         $url = sprintf('https://api.openai.com/v1/%s', $endpoint);
 
-        try {
-            $response = $this->httpClient->request('POST', $url, [
-                'auth_bearer' => $this->apiKey,
-                'headers' => ['Content-Type' => 'application/json'],
-                'body' => json_encode($body),
-            ]);
+        $response = $this->httpClient->request('POST', $url, [
+            'auth_bearer' => $this->apiKey,
+            'headers' => ['Content-Type' => 'application/json'],
+            'body' => json_encode($body),
+        ]);
 
-            return $response->toArray();
-        } catch (ClientException $exception) {
-            $response = $exception->getResponse();
-            var_dump($response->getContent(false));
-
-            throw $exception;
-        }
+        return $response->toArray();
     }
 }
