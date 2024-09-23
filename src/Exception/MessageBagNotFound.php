1c1ed9a0cc8e6845d1fa185daba6a8ea2ee4d7cf
Oskar Stark <oskarstark@googlemail.com>
2024-09-23 23:54:12 +0200

    chore: more specific exception (#24)
    * More specific exception

--- src/Exception/MessageBagNotFound.php
+++ src/Exception/MessageBagNotFound.php
@@ -6,7 +6,7 @@ namespace PhpLlm\LlmChain\Exception;
 
 final class MessageBagNotFound extends \DomainException
 {
-    public static function notFound(string $id): self
+    public static function withId(string $id): self
     {
         return new self(sprintf('MessageBag with id "%s" not found', $id));
     }
