088ac617572f788885aae068a58cbef53bbfabb7
Oskar Stark <oskarstark@googlemail.com>
2024-09-23 18:24:21 +0200

    More specific exception

--- src/Exception/InvalidToolImplementation.php
+++ src/Exception/InvalidToolImplementation.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace PhpLlm\LlmChain\Exception;
 
+use PhpLlm\LlmChain\ToolBox\AsTool;
+
 final class InvalidToolImplementation extends \InvalidArgumentException
 {
     public static function missingAttribute(string $className): self
     {
-        return new self(sprintf('The class "%s" is not a tool, please add AsTool attribute.', $className));
+        return new self(sprintf('The class "%s" is not a tool, please add %s attribute.', $className, AsTool::class));
     }
 }
