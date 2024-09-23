dccc6dd9a973459b04046cf848d95ab1d0e7f21f
Oskar Stark <oskarstark@googlemail.com>
2024-09-23 13:47:28 +0200

    chore: helpful exceptions (#17)
    * Helpful exceptions

--- src/ToolBox/Tool/YouTubeTranscriber.php
+++ src/ToolBox/Tool/YouTubeTranscriber.php
@@ -16,10 +16,10 @@ final readonly class YouTubeTranscriber
         private HttpClientInterface $client,
     ) {
         if (!class_exists(Crawler::class)) {
-            throw new \LogicException('The Symfony DomCrawler component is required to use this tool.');
+            throw new \LogicException('The Symfony DomCrawler component is required to use this tool. Try running "composer require symfony/dom-crawler".');
         }
         if (!class_exists(CssSelectorConverter::class)) {
-            throw new \LogicException('The Symfony CSS Selector component is required to use this tool.');
+            throw new \LogicException('The Symfony CSS Selector component is required to use this tool. Try running "composer require symfony/css-selector".');
         }
     }
 
