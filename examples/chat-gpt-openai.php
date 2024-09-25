5c51c0ce05bbe778c39957b5b929938e85dc0e35
Oskar Stark <oskarstark@googlemail.com>
2024-09-25 22:56:23 +0200

    Apply suggestions from code review

--- examples/chat-gpt-openai.php
+++ examples/chat-gpt-openai.php
@@ -12,7 +12,7 @@ require_once dirname(__DIR__).'/vendor/autoload.php';
 
 $platform = new OpenAI(HttpClient::create(), getenv('OPENAI_API_KEY'));
 $llm = new Gpt($platform, Version::gpt4oMini(), [
-    'temperature' => 0.5,
+    'temperature' => 0.5, // default options for the model
 ]);
 
 $chain = new Chain($llm);
@@ -21,7 +21,7 @@ $messages = new MessageBag(
     Message::ofUser('What is the Symfony framework?'),
 );
 $response = $chain->call($messages, [
-    'max_tokens' => 500,
+    'max_tokens' => 500, // specific options just for this call
 ]);
 
 echo $response.PHP_EOL;
