f5accab3dbfe06610f7865e2ba31a7eba6c0d7cd
Oskar Stark <oskarstark@googlemail.com>
2024-09-25 22:36:43 +0200

    feat: Read description for schema to give more context to the LLM (#38)
    * Read description for schema to gove more context to the LLM
    
    * -

--- tests/StructuredOutput/Data/User.php
+++ tests/StructuredOutput/Data/User.php
@@ -7,6 +7,9 @@ namespace PhpLlm\LlmChain\Tests\StructuredOutput\Data;
 final class User
 {
     public int $id;
+    /**
+     * @var string The name of the user in lowercase
+     */
     public string $name;
     public \DateTimeInterface $createdAt;
     public bool $isActive;
