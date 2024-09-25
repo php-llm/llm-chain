bcb7e121501adaaa7ed9b76dc48736750ffe65e8
Denis Zunke <denis.zunke@gmail.com>
2024-09-25 16:50:52 +0200

    Fix image describer after latest interface change

--- examples/image-describer.php
+++ examples/image-describer.php
@@ -7,6 +7,7 @@ use PhpLlm\LlmChain\OpenAI\Model\Gpt;
 use PhpLlm\LlmChain\OpenAI\Model\Gpt\Version;
 use PhpLlm\LlmChain\OpenAI\Runtime\OpenAI;
 use Symfony\Component\HttpClient\HttpClient;
+use PhpLlm\LlmChain\Message\Content\Image;
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
@@ -18,8 +19,8 @@ $messages = new MessageBag(
     Message::forSystem('You are an image analyzer bot that helps identify the content of images.'),
     Message::ofUser(
         'Describe the images as a comedian would do it.',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Webysther_20160423_-_Elephpant.svg/350px-Webysther_20160423_-_Elephpant.svg.png',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/320px-African_Bush_Elephant.jpg',
+        new Image('https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Webysther_20160423_-_Elephpant.svg/350px-Webysther_20160423_-_Elephpant.svg.png'),
+		new Image('https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/320px-African_Bush_Elephant.jpg'),
     ),
 );
 $response = $chain->call($messages);
