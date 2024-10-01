8ba3e1577cc8bdc099a249d619fdf08b53b557a8
Oskar Stark <oskarstark@googlemail.com>
2024-10-01 18:20:03 +0200

    Use `symfony/clock` in `DocumentEmbedder`

--- tests/DocumentEmbedderTest.php
+++ tests/DocumentEmbedderTest.php
@@ -16,6 +16,7 @@ use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Clock\MockClock;
 use Symfony\Component\Uid\Uuid;
 
 #[CoversClass(DocumentEmbedder::class)]
@@ -24,6 +25,7 @@ final class DocumentEmbedderTest extends TestCase
 {
     private EmbeddingsModel&MockObject $embeddings;
     private StoreInterface&MockObject $store;
+    private MockClock $clock;
     private LoggerInterface&MockObject $logger;
     private DocumentEmbedder $embedder;
 
@@ -31,8 +33,14 @@ final class DocumentEmbedderTest extends TestCase
     {
         $this->embeddings = $this->createMock(EmbeddingsModel::class);
         $this->store = $this->createMock(StoreInterface::class);
+        $this->clock = new MockClock('2024-01-01 00:00:00');
         $this->logger = $this->createMock(LoggerInterface::class);
-        $this->embedder = new DocumentEmbedder($this->embeddings, $this->store, $this->logger);
+        $this->embedder = new DocumentEmbedder(
+            $this->embeddings,
+            $this->store,
+            $this->clock,
+            $this->logger,
+        );
     }
 
     #[Test]
@@ -92,5 +100,27 @@ final class DocumentEmbedderTest extends TestCase
         }));
 
         $this->embedder->embed($document);
+    }
+
+    #[Test]
+    public function embedWithSleep(): void
+    {
+        $vectorData = [0.1, 0.2, 0.3];
+        $document1 = new Document(Uuid::v4(), 'Test content 1', $vector1 = new Vector($vectorData));
+        $document2 = new Document(Uuid::v4(), 'Test content 2', $vector2 = new Vector($vectorData));
+
+        $this->embeddings->method('multiCreate')->willReturn([$vector1, $vector2]);
+
+        $this->store->expects($this->once())->method('addDocuments')->with([$document1, $document2]);
+
+        $this->embedder->embed(
+            documents: [$document1, $document2],
+            sleep: 3
+        );
+
+        $this->assertSame(
+            '2024-01-01 00:00:03',
+            $this->clock->now()->format('Y-m-d H:i:s')
+        );
     }
 }
