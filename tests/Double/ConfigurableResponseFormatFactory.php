d9671a576eb754b119a0485455e6acdbb0eee954
Oskar Stark <oskarstark@googlemail.com>
2024-10-02 11:05:30 +0200

    tests: Add `ChainProcessorTest`

---
+++ tests/Double/ConfigurableResponseFormatFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpLlm\LlmChain\Tests\Double;
+
+use PhpLlm\LlmChain\StructuredOutput\ResponseFormatFactoryInterface;
+
+final readonly class ConfigurableResponseFormatFactory implements ResponseFormatFactoryInterface
+{
+    /**
+     * @param array<mixed> $responseFormat
+     */
+    public function __construct(
+        private array $responseFormat = [],
+    ) {
+    }
+
+    public function create(string $responseClass): array
+    {
+        return $this->responseFormat;
+    }
+}
