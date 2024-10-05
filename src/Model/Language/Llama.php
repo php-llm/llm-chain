59b442f5c76068f8f54e83879c809d486bd4bf5f
Oskar Stark <oskarstark@googlemail.com>
2024-10-05 22:40:53 +0200

    LLama prompt

--- src/Model/Language/Llama.php
+++ src/Model/Language/Llama.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 namespace PhpLlm\LlmChain\Model\Language;
 
 use PhpLlm\LlmChain\LanguageModel;
+use PhpLlm\LlmChain\Message\AssistantMessage;
+use PhpLlm\LlmChain\Message\Message;
 use PhpLlm\LlmChain\Message\MessageBag;
+use PhpLlm\LlmChain\Message\MessageInterface;
+use PhpLlm\LlmChain\Message\UserMessage;
 use PhpLlm\LlmChain\Platform\Ollama;
 use PhpLlm\LlmChain\Platform\Replicate;
 use PhpLlm\LlmChain\Response\TextResponse;
@@ -22,12 +26,35 @@ final readonly class Llama implements LanguageModel
         $systemMessage = $messages->getSystemMessage();
         $endpoint = $this->platform instanceof Replicate ? 'predictions' : 'chat';
 
+
         $response = $this->platform->request('meta/meta-llama-3.1-405b-instruct', $endpoint, [
             'system' => $systemMessage?->content,
-            'prompt' => $messages->withoutSystemMessage()->getIterator()->current()->content[0]->text, // @phpstan-ignore-line TODO: Multiple messages
+            'prompt' => self::convertToPrompt($messages->withoutSystemMessage()),
         ]);
 
         return new TextResponse(implode('', $response['output']));
+    }
+
+    private static function convertToPrompt(MessageBag $messageBag): string
+    {
+        $messages = [];
+
+        /** @var MessageInterface $message */
+        foreach ($messageBag->getIterator() as $message) {
+            if ($message instanceof UserMessage) {
+                $content = $message->content[0]->text;
+            }
+
+            elseif ($message instanceof AssistantMessage && $message->content !== null) {
+                $content = $message->content;
+            } else {
+                continue;
+            }
+
+            $messages[] = sprintf('%s: %s', ucfirst($message->getRole()->value), $message->content[0]->text);
+        }
+
+        return implode(PHP_EOL, $messages);
     }
 
     public function supportsToolCalling(): bool
