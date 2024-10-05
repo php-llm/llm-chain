9a6b3d9690f7a4619011b459fbe31e40ad942fa4
Oskar Stark <oskarstark@googlemail.com>
2024-10-05 10:22:25 +0200

    Add tests

---
+++ tests/Response/ChoiceResponseTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpLlm\LlmChain\Tests\Response;
+
+use PhpLlm\LlmChain\Response\Choice;
+use PhpLlm\LlmChain\Response\ChoiceResponse;
+use PhpLlm\LlmChain\Exception\InvalidArgumentException;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Small;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(ChoiceResponse::class)]
+#[Small]
+final class ChoiceResponseTest extends TestCase
+{
+    #[Test]
+    public function testChoiceResponseCreation(): void
+    {
+        $choice1 = new Choice('choice1');
+        $choice2 = new Choice(null);
+        $choice3 = new Choice('choice3');
+        $response = new ChoiceResponse($choice1, $choice2, $choice3);
+
+        $this->assertCount(3, $response->getContent());
+        $this->assertSame('choice1', $response->getContent()[0]->getContent());
+        $this->assertNull($response->getContent()[1]->getContent());
+        $this->assertSame('choice3', $response->getContent()[2]->getContent());
+    }
+
+    #[Test]
+    public function testChoiceResponseWithNoChoices(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Response must have at least one choice.');
+
+        new ChoiceResponse();
+    }
+}
