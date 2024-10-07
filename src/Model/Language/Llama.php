308e8f865d961b1e4df976e615fb98986064d6f4
Oskar Stark <oskarstark@googlemail.com>
2024-10-07 08:28:08 +0200

    Llama prompt

--- src/Model/Language/Llama.php
+++ src/Model/Language/Llama.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace PhpLlm\LlmChain\Model\Language;
 
+use PhpLlm\LlmChain\Exception\RuntimeException;
 use PhpLlm\LlmChain\LanguageModel;
+use PhpLlm\LlmChain\Message\AssistantMessage;
 use PhpLlm\LlmChain\Message\MessageBag;
+use PhpLlm\LlmChain\Message\MessageInterface;
+use PhpLlm\LlmChain\Message\SystemMessage;
+use PhpLlm\LlmChain\Message\UserMessage;
 use PhpLlm\LlmChain\Platform\Ollama;
 use PhpLlm\LlmChain\Platform\Replicate;
 use PhpLlm\LlmChain\Response\TextResponse;
@@ -23,11 +28,45 @@ final readonly class Llama implements LanguageModel
         $endpoint = $this->platform instanceof Replicate ? 'predictions' : 'chat';
 
         $response = $this->platform->request('meta/meta-llama-3.1-405b-instruct', $endpoint, [
-            'system' => $systemMessage?->content,
-            'prompt' => $messages->withoutSystemMessage()->getIterator()->current()->content[0]->text, // @phpstan-ignore-line TODO: Multiple messages
+            'system' => self::convertMessage($systemMessage ?? new SystemMessage('')),
+            'prompt' => self::convertToPrompt($messages->withoutSystemMessage()),
         ]);
 
         return new TextResponse(implode('', $response['output']));
+    }
+
+    private static function convertToPrompt(MessageBag $messageBag): string
+    {
+        $messages = [];
+
+        /** @var UserMessage|SystemMessage|AssistantMessage $message */
+        foreach ($messageBag->getIterator() as $message) {
+            $messages[] = self::convertMessage($message);
+        }
+
+        return implode(PHP_EOL.PHP_EOL, $messages);
+    }
+
+    private static function convertMessage(UserMessage|SystemMessage|AssistantMessage $message): string
+    {
+        if ($message instanceof SystemMessage) {
+            return $prompt = <<<SYSTEM
+<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+{$message->content}
+
+SYSTEM;
+        }
+
+        if ($message instanceof UserMessage || $message instanceof AssistantMessage) {
+            return <<<USER
+<|start_header_id|>{$message->getRole()->value}<|end_header_id|>
+{$message->content}
+<|eot_id|>
+
+USER;
+        }
+
+        throw new RuntimeException('Unsupported message type.');
     }
 
     public function supportsToolCalling(): bool
