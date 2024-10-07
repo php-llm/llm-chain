9adbe339becc5f7e614ad29176a2fae20e6c4ae4
Oskar Stark <oskarstark@googlemail.com>
2024-10-07 11:40:43 +0200

    -

--- src/Model/Language/Llama.php
+++ src/Model/Language/Llama.php
@@ -7,8 +7,9 @@ namespace PhpLlm\LlmChain\Model\Language;
 use PhpLlm\LlmChain\Exception\RuntimeException;
 use PhpLlm\LlmChain\LanguageModel;
 use PhpLlm\LlmChain\Message\AssistantMessage;
+use PhpLlm\LlmChain\Message\Content\Image;
+use PhpLlm\LlmChain\Message\Content\Text;
 use PhpLlm\LlmChain\Message\MessageBag;
-use PhpLlm\LlmChain\Message\MessageInterface;
 use PhpLlm\LlmChain\Message\SystemMessage;
 use PhpLlm\LlmChain\Message\UserMessage;
 use PhpLlm\LlmChain\Platform\Ollama;
@@ -47,7 +48,7 @@ final readonly class Llama implements LanguageModel
             $messages[] = self::convertMessage($message);
         }
 
-        return implode(PHP_EOL.PHP_EOL, $messages);
+        return trim(implode(PHP_EOL.PHP_EOL, $messages));
     }
 
     /**
@@ -56,23 +57,62 @@ final readonly class Llama implements LanguageModel
     public static function convertMessage(UserMessage|SystemMessage|AssistantMessage $message): string
     {
         if ($message instanceof SystemMessage) {
-            return $prompt = <<<SYSTEM
+            return trim(<<<SYSTEM
 <|begin_of_text|><|start_header_id|>system<|end_header_id|>
-{$message->content}
 
-SYSTEM;
+{$message->content}<|eot_id|>
+SYSTEM);
         }
 
-        if ($message instanceof UserMessage || $message instanceof AssistantMessage) {
-            return <<<USER
+        if ($message instanceof AssistantMessage) {
+            if ('' === $message->content || null === $message->content) {
+                return '';
+            }
+
+            return trim(<<<ASSISTANT
 <|start_header_id|>{$message->getRole()->value}<|end_header_id|>
-{$message->content}
-<|eot_id|>
 
-USER;
+{$message->content}<|eot_id|>
+ASSISTANT);
         }
 
-        throw new RuntimeException('Unsupported message type.');
+        if ($message instanceof UserMessage) {
+            $count = count($message->content);
+
+            $contentParts = [];
+            if ($count > 1) {
+                foreach ($message->content as $value) {
+                    if ($value instanceof Text) {
+                        $contentParts[] = $value->text;
+                    }
+
+                    if ($value instanceof Image) {
+                        $contentParts[] = $value->url;
+                    }
+                }
+            } elseif (1 === $count) {
+                $value = $message->content[0];
+                if ($value instanceof Text) {
+                    $contentParts[] = $value->text;
+                }
+
+                if ($value instanceof Image) {
+                    $contentParts[] = $value->url;
+                }
+            } else {
+                throw new RuntimeException('Unsupported message type.');
+            }
+
+            $content = implode(PHP_EOL, $contentParts);
+
+            return trim(<<<USER
+<|start_header_id|>{$message->getRole()->value}<|end_header_id|>
+
+{$content}<|eot_id|>
+USER);
+        }
+
+        throw new RuntimeException('Unsupported message type.'); // @phpstan-ignore-line
     }
 
     public function supportsToolCalling(): bool
--- tests/Model/Language/LlamaTest.php
+++ tests/Model/Language/LlamaTest.php
@@ -4,20 +4,18 @@ declare(strict_types=1);
 
 namespace PhpLlm\LlmChain\Tests\Model\Language;
 
-use PhpLlm\LlmChain\Exception\InvalidArgumentException;
 use PhpLlm\LlmChain\Message\AssistantMessage;
-use PhpLlm\LlmChain\Message\MessageInterface;
+use PhpLlm\LlmChain\Message\Content\Image;
+use PhpLlm\LlmChain\Message\Message;
+use PhpLlm\LlmChain\Message\MessageBag;
 use PhpLlm\LlmChain\Message\SystemMessage;
 use PhpLlm\LlmChain\Message\UserMessage;
 use PhpLlm\LlmChain\Model\Language\Llama;
 use PhpLlm\LlmChain\Platform\Ollama;
-use PhpLlm\LlmChain\Response\Choice;
-use PhpLlm\LlmChain\Response\ChoiceResponse;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Small;
 use PHPUnit\Framework\Attributes\Test;
-use PHPUnit\Framework\Attributes\UsesClass;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpClient\MockHttpClient;
 
@@ -26,35 +24,110 @@ use Symfony\Component\HttpClient\MockHttpClient;
 final class LlamaTest extends TestCase
 {
     #[Test]
-    #[DataProvider('provideMessages')]
-    public function convertMessage(string $expected, UserMessage|SystemMessage|AssistantMessage $message): void
+    public function convertMessages(): void
     {
-        (new Llama(new Ollama(new MockHttpClient(), 'http://example.com')))->convertMessage($message);
-    }
+        $messageBag = new MessageBag(
+        );
+        foreach (self::provideMessages() as $message) {
+            $messageBag->append($message[1]);
+        }
 
-    public static function provideMessages(): iterable
-    {
-        yield 'User message' => [
-            'expected' => <<<USER
-<|begin_of_text|><|start_header_id|>user<|end_header_id|>
+        $llama = new Llama(new Ollama(new MockHttpClient(), 'http://example.com'));
+
+        self::assertSame(<<<EXPECTED
+<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+
+You are a helpful chatbot.<|eot_id|>
+
+<|start_header_id|>user<|end_header_id|>
+
+Hello, how are you?<|eot_id|>
+
+<|start_header_id|>user<|end_header_id|>
+
 Hello, how are you?
-USER,
-            'message' => new UserMessage(new TextConten'Hello, how are you?'),
-        ];
+What is your name?<|eot_id|>
 
-        yield 'System message' => [
-            'expected' => <<<SYSTEM
-USER;
+<|start_header_id|>user<|end_header_id|>
 
-        
+Hello, how are you?
+What is your name?
+https://example.com/image.jpg<|eot_id|>
+
+<|start_header_id|>assistant<|end_header_id|>
+
+I am an assistant.<|eot_id|>
+EXPECTED,
+            $llama->convertToPrompt($messageBag)
+        );
     }
 
     #[Test]
-    public function choiceResponseWithNoChoices(): void
+    #[DataProvider('provideMessages')]
+    public function convertMessage(string $expected, UserMessage|SystemMessage|AssistantMessage $message): void
     {
-        $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('Response must have at least one choice.');
+        self::assertSame(
+            $expected,
+            (new Llama(new Ollama(new MockHttpClient(), 'http://example.com')))->convertMessage($message)
+        );
+    }
 
-        new ChoiceResponse();
+    /**
+     * @return iterable<array{string, UserMessage|SystemMessage|AssistantMessage}>
+     */
+    public static function provideMessages(): iterable
+    {
+        yield 'System message' => [
+            <<<SYSTEM
+<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+
+You are a helpful chatbot.<|eot_id|>
+SYSTEM,
+            Message::forSystem('You are a helpful chatbot.'),
+        ];
+
+        yield 'UserMessage' => [
+            <<<USER
+<|start_header_id|>user<|end_header_id|>
+
+Hello, how are you?<|eot_id|>
+USER,
+            Message::ofUser('Hello, how are you?'),
+        ];
+
+        yield 'UserMessage with two texts' => [
+            <<<USER
+<|start_header_id|>user<|end_header_id|>
+
+Hello, how are you?
+What is your name?<|eot_id|>
+USER,
+            Message::ofUser('Hello, how are you?', 'What is your name?'),
+        ];
+
+        yield 'UserMessage with two texts and one image' => [
+            <<<USER
+<|start_header_id|>user<|end_header_id|>
+
+Hello, how are you?
+What is your name?
+https://example.com/image.jpg<|eot_id|>
+USER,
+            Message::ofUser('Hello, how are you?', 'What is your name?', new Image('https://example.com/image.jpg')),
+        ];
+
+        yield 'AssistantMessage' => [
+            <<<ASSISTANT
+<|start_header_id|>assistant<|end_header_id|>
+
+I am an assistant.<|eot_id|>
+ASSISTANT,
+            new AssistantMessage('I am an assistant.'),
+        ];
+
+        yield 'AssistantMessage with null content' => [
+            '',
+            new AssistantMessage(),
+        ];
     }
 }
