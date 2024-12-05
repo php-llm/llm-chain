e97ed42984938aa01aaec6529e0b3cc051d0356b
Denis Zunke <denis.zunke@gmail.com>
2024-12-05 20:17:38 +0100

    Parse OpenAI Response embedding to contain all embeddings

---
+++ tests/Bridge/OpenAI/Embeddings/ResponseConverterTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpLlm\LlmChain\Tests\Bridge\OpenAI\Embeddings;
+
+use PhpLlm\LlmChain\Bridge\OpenAI\Embeddings\ResponseConverter;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Small;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+#[CoversClass(ResponseConverter::class)]
+#[Small]
+class ResponseConverterTest extends TestCase
+{
+    #[Test]
+    public function itConvertsAResponseToAVectorResponse(): void
+    {
+        $response = $this->createStub(ResponseInterface::class);
+        $response
+            ->method('toArray')
+            ->willReturn(\json_decode($this->getEmbeddingStub(), true));
+
+        $vectorResponse = (new ResponseConverter())->convert($response);
+        $convertedContent = $vectorResponse->getContent();
+
+        self::assertIsArray($convertedContent);
+        self::assertCount(2, $convertedContent);
+
+        self::assertSame([0.3, 0.4, 0.4], $convertedContent[0]->getData());
+        self::assertSame([0.0, 0.0, 0.2], $convertedContent[1]->getData());
+    }
+
+    private function getEmbeddingStub(): string
+    {
+        return <<<'JSON'
+        {
+          "object": "list",
+          "data": [
+            {
+              "object": "embedding",
+              "index": 0,
+              "embedding": [0.3, 0.4, 0.4]
+            },
+            {
+              "object": "embedding",
+              "index": 1,
+              "embedding": [0.0, 0.0, 0.2]
+            }
+          ]
+        }
+        JSON;
+    }
+}
