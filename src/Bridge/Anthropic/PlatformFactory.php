8b88d5d6effa7af8c2ef2d487c19a3e09013fa39
Christopher Hertel <mail@christopher-hertel.de>
2024-12-08 02:30:55 +0100

    fix: make it possible to inject http client into factories (#155)

--- src/Bridge/Anthropic/PlatformFactory.php
+++ src/Bridge/Anthropic/PlatformFactory.php
@@ -6,12 +6,18 @@ namespace PhpLlm\LlmChain\Bridge\Anthropic;
 
 use PhpLlm\LlmChain\Platform;
 use Symfony\Component\HttpClient\EventSourceHttpClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 final readonly class PlatformFactory
 {
-    public static function create(#[\SensitiveParameter] string $apiKey, string $version = '2023-06-01'): Platform
-    {
-        $responseHandler = new ModelHandler(new EventSourceHttpClient(), $apiKey, $version);
+    public static function create(
+        #[\SensitiveParameter]
+        string $apiKey,
+        string $version = '2023-06-01',
+        ?HttpClientInterface $httpClient = null,
+    ): Platform {
+        $httpClient = $httpClient instanceof EventSourceHttpClient ? $httpClient : new EventSourceHttpClient($httpClient);
+        $responseHandler = new ModelHandler($httpClient, $apiKey, $version);
 
         return new Platform([$responseHandler], [$responseHandler]);
     }
--- src/Bridge/Replicate/PlatformFactory.php
+++ src/Bridge/Replicate/PlatformFactory.php
@@ -7,12 +7,19 @@ namespace PhpLlm\LlmChain\Bridge\Replicate;
 use PhpLlm\LlmChain\Bridge\Meta\LlamaPromptConverter;
 use PhpLlm\LlmChain\Platform;
 use Symfony\Component\Clock\Clock;
+use Symfony\Component\HttpClient\EventSourceHttpClient;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 final class PlatformFactory
 {
-    public static function create(string $apiKey): Platform
-    {
+    public static function create(
+        #[\SensitiveParameter]
+        string $apiKey,
+        ?HttpClientInterface $httpClient = null,
+    ): Platform {
+        $httpClient = $httpClient instanceof EventSourceHttpClient ? $httpClient : new EventSourceHttpClient($httpClient);
+
         return new Platform(
             [new LlamaModelClient(new Client(HttpClient::create(), new Clock(), $apiKey), new LlamaPromptConverter())],
             [new LlamaResponseConverter()],
