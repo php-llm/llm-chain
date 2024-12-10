c6ac0e711f2adec5e94ad52bf29884813462ed04
Christopher Hertel <mail@christopher-hertel.de>
2024-12-11 00:50:14 +0100

    feat: improve and test wikipedia tool (#157)

--- src/Chain/ToolBox/Tool/Wikipedia.php
+++ src/Chain/ToolBox/Tool/Wikipedia.php
@@ -8,7 +8,7 @@ use PhpLlm\LlmChain\Chain\ToolBox\Attribute\AsTool;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[AsTool('wikipedia_search', description: 'Searches Wikipedia for a given query', method: 'search')]
-#[AsTool('wikipedia_article', description: 'Retrieves a Wikipedia article by its title', method: 'getArticle')]
+#[AsTool('wikipedia_article', description: 'Retrieves a Wikipedia article by its title', method: 'article')]
 final readonly class Wikipedia
 {
     public function __construct(
@@ -31,13 +31,22 @@ final readonly class Wikipedia
 
         $titles = array_map(fn (array $item) => $item['title'], $result['query']['search']);
 
-        return 'On Wikipedia, I found the following articles: '.implode(', ', $titles).'.';
+        if (empty($titles)) {
+            return 'No articles were found on Wikipedia.';
+        }
+
+        $response = 'Articles with the following titles were found on Wikipedia:'.PHP_EOL;
+        foreach ($titles as $title) {
+            $response .= ' - '.$title.PHP_EOL;
+        }
+
+        return $response.PHP_EOL.'Use the title of the article with tool "wikipedia_article" to load the content.';
     }
 
     /**
-     * @param string $title The title of the article to retrieve from Wikipedia
+     * @param string $title The title of the article to load from Wikipedia
      */
-    public function getArticle(string $title): string
+    public function article(string $title): string
     {
         $result = $this->execute([
             'action' => 'query',
@@ -45,9 +54,24 @@ final readonly class Wikipedia
             'prop' => 'extracts|info|pageimages',
             'titles' => $title,
             'explaintext' => true,
+            'redirects' => true,
         ], $this->locale);
 
-        return current($result['query']['pages'])['extract'];
+        $article = current($result['query']['pages']);
+
+        if (array_key_exists('missing', $article)) {
+            return sprintf('No article with title "%s" was found on Wikipedia.', $title);
+        }
+
+        $response = '';
+        if (array_key_exists('redirects', $result['query'])) {
+            foreach ($result['query']['redirects'] as $redirect) {
+                $response .= sprintf('The article "%s" redirects to article "%s".', $redirect['from'], $redirect['to']).PHP_EOL;
+            }
+            $response .= PHP_EOL;
+        }
+
+        return $response.'This is the content of article "'.$article['title'].'":'.PHP_EOL.$article['extract'];
     }
 
     /**
