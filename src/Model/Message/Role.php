62ff661ed7c0809e81a512a88ad658087fc4546b
Oskar Stark <oskarstark@googlemail.com>
2024-12-21 02:43:30 +0100

    feat: add `Comparable` trait from `oskarstark/enum-helper` (#163)
    * Add `Comparable` trait from `oskarstark/enum-helper`
    
    see https://github.com/OskarStark/enum-helper
    
    * -

--- src/Model/Message/Role.php
+++ src/Model/Message/Role.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace PhpLlm\LlmChain\Model\Message;
 
+use OskarStark\Enum\Trait\Comparable;
+
 enum Role: string
 {
+    use Comparable;
+
     case System = 'system';
     case Assistant = 'assistant';
     case User = 'user';
