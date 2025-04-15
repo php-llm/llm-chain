678c9ca37cc7c41ffff87c89d1023ed85709b869
Denis Zunke <denis.zunke@gmail.com>
2025-04-15 06:56:47 +0200

    feat: add new v4.1 models

--- src/Bridge/OpenAI/GPT.php
+++ src/Bridge/OpenAI/GPT.php
@@ -20,6 +20,32 @@ final class GPT implements LanguageModel
     public const O3_MINI = 'o3-mini';
     public const O3_MINI_HIGH = 'o3-mini-high';
     public const GPT_45_PREVIEW = 'gpt-4.5-preview';
+    public const GPT_41 = 'gpt-4.1';
+    public const GPT_41_MINI = 'gpt-4.1-mini';
+    public const GPT_41_NANO = 'gpt-4.1-nano';
+
+    private const IMAGE_SUPPORTING = [
+        self::GPT_4_TURBO,
+        self::GPT_4O,
+        self::GPT_4O_MINI,
+        self::O1_MINI,
+        self::O1_PREVIEW,
+        self::O3_MINI,
+        self::GPT_45_PREVIEW,
+        self::GPT_41,
+        self::GPT_41_MINI,
+        self::GPT_41_NANO,
+    ];
+
+    private const STRUCTURED_OUTPUT_SUPPORTING = [
+        self::GPT_4O,
+        self::GPT_4O_MINI,
+        self::O3_MINI,
+        self::GPT_45_PREVIEW,
+        self::GPT_41,
+        self::GPT_41_MINI,
+        self::GPT_41_NANO,
+    ];
 
     /**
      * @param array<mixed> $options The default options for the model usage
@@ -36,11 +62,11 @@ final class GPT implements LanguageModel
         }
 
         if (false === $this->supportsImageInput) {
-            $this->supportsImageInput = in_array($this->name, [self::GPT_4_TURBO, self::GPT_4O, self::GPT_4O_MINI, self::O1_MINI, self::O1_PREVIEW, self::O3_MINI, self::GPT_45_PREVIEW], true);
+            $this->supportsImageInput = in_array($this->name, self::IMAGE_SUPPORTING, true);
         }
 
         if (false === $this->supportsStructuredOutput) {
-            $this->supportsStructuredOutput = in_array($this->name, [self::GPT_4O, self::GPT_4O_MINI, self::O3_MINI, self::GPT_45_PREVIEW], true);
+            $this->supportsStructuredOutput = in_array($this->name, self::STRUCTURED_OUTPUT_SUPPORTING, true);
         }
     }
 
