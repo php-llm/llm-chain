c885b22cb27a0aaab09708dc69f454e5cb96bad9
Christopher Hertel <mail@christopher-hertel.de>
2025-05-04 19:32:36 +0200

    feat: extend Claude support for images and pdf input

--- src/Bridge/Anthropic/ModelClient.php
+++ src/Bridge/Anthropic/ModelClient.php
@@ -5,27 +5,19 @@ declare(strict_types=1);
 namespace PhpLlm\LlmChain\Bridge\Anthropic;
 
 use PhpLlm\LlmChain\Chain\Toolbox\Metadata;
-use PhpLlm\LlmChain\Exception\RuntimeException;
 use PhpLlm\LlmChain\Model\Message\AssistantMessage;
 use PhpLlm\LlmChain\Model\Message\MessageBagInterface;
 use PhpLlm\LlmChain\Model\Message\MessageInterface;
 use PhpLlm\LlmChain\Model\Message\ToolCallMessage;
 use PhpLlm\LlmChain\Model\Model;
-use PhpLlm\LlmChain\Model\Response\ResponseInterface as LlmResponse;
-use PhpLlm\LlmChain\Model\Response\StreamResponse;
-use PhpLlm\LlmChain\Model\Response\TextResponse;
 use PhpLlm\LlmChain\Model\Response\ToolCall;
-use PhpLlm\LlmChain\Model\Response\ToolCallResponse;
-use PhpLlm\LlmChain\Platform\ModelClient;
-use PhpLlm\LlmChain\Platform\ResponseConverter;
-use Symfony\Component\HttpClient\Chunk\ServerSentEvent;
+use PhpLlm\LlmChain\Platform\ModelClient as PlatformModelClient;
 use Symfony\Component\HttpClient\EventSourceHttpClient;
-use Symfony\Component\HttpClient\Exception\JsonException;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
 use Webmozart\Assert\Assert;
 
-final readonly class ModelHandler implements ModelClient, ResponseConverter
+final readonly class ModelClient implements PlatformModelClient
 {
     private EventSourceHttpClient $httpClient;
 
@@ -107,56 +99,5 @@ final readonly class ModelHandler implements ModelClient, ResponseConverter
             ],
             'json' => array_merge($options, $body),
         ]);
-    }
-
-    public function convert(ResponseInterface $response, array $options = []): LlmResponse
-    {
-        if ($options['stream'] ?? false) {
-            return new StreamResponse($this->convertStream($response));
-        }
-
-        $data = $response->toArray();
-
-        if (!isset($data['content']) || 0 === count($data['content'])) {
-            throw new RuntimeException('Response does not contain any content');
-        }
-
-        if (!isset($data['content'][0]['text'])) {
-            throw new RuntimeException('Response content does not contain any text');
-        }
-
-        $toolCalls = [];
-        foreach ($data['content'] as $content) {
-            if ('tool_use' === $content['type']) {
-                $toolCalls[] = new ToolCall($content['id'], $content['name'], $content['input']);
-            }
-        }
-        if (!empty($toolCalls)) {
-            return new ToolCallResponse(...$toolCalls);
-        }
-
-        return new TextResponse($data['content'][0]['text']);
-    }
-
-    private function convertStream(ResponseInterface $response): \Generator
-    {
-        foreach ((new EventSourceHttpClient())->stream($response) as $chunk) {
-            if (!$chunk instanceof ServerSentEvent || '[DONE]' === $chunk->getData()) {
-                continue;
-            }
-
-            try {
-                $data = $chunk->getArrayData();
-            } catch (JsonException) {
-                // try catch only needed for Symfony 6.4
-                continue;
-            }
-
-            if ('content_block_delta' != $data['type'] || !isset($data['delta']['text'])) {
-                continue;
-            }
-
-            yield $data['delta']['text'];
-        }
     }
 }
