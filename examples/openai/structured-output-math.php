245e53f979454387579cd765238d0e8a53a6229e
Christopher Hertel <mail@christopher-hertel.de>
2025-05-16 15:21:38 +0200

    fix: implement failing test and cleanup (#308)

--- examples/openai/structured-output-math.php
+++ examples/openai/structured-output-math.php
@@ -4,14 +4,10 @@ use PhpLlm\LlmChain\Bridge\OpenAI\GPT;
 use PhpLlm\LlmChain\Bridge\OpenAI\PlatformFactory;
 use PhpLlm\LlmChain\Chain;
 use PhpLlm\LlmChain\Chain\StructuredOutput\ChainProcessor;
-use PhpLlm\LlmChain\Chain\StructuredOutput\ResponseFormatFactory;
 use PhpLlm\LlmChain\Model\Message\Message;
 use PhpLlm\LlmChain\Model\Message\MessageBag;
 use PhpLlm\LlmChain\Tests\Fixture\StructuredOutput\MathReasoning;
 use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 
 require_once dirname(__DIR__, 2).'/vendor/autoload.php';
 (new Dotenv())->loadEnv(dirname(__DIR__, 2).'/.env');
@@ -23,9 +19,8 @@ if (empty($_ENV['OPENAI_API_KEY'])) {
 
 $platform = PlatformFactory::create($_ENV['OPENAI_API_KEY']);
 $llm = new GPT(GPT::GPT_4O_MINI);
-$serializer = new Serializer([new ObjectNormalizer()], [new JsonEncoder()]);
 
-$processor = new ChainProcessor(new ResponseFormatFactory(), $serializer);
+$processor = new ChainProcessor();
 $chain = new Chain($platform, $llm, [$processor], [$processor]);
 $messages = new MessageBag(
     Message::forSystem('You are a helpful math tutor. Guide the user through the solution step by step.'),
--- src/Chain/StructuredOutput/ChainProcessor.php
+++ src/Chain/StructuredOutput/ChainProcessor.php
@@ -11,6 +11,12 @@ use PhpLlm\LlmChain\Chain\OutputProcessor;
 use PhpLlm\LlmChain\Exception\InvalidArgumentException;
 use PhpLlm\LlmChain\Exception\MissingModelSupport;
 use PhpLlm\LlmChain\Model\Response\StructuredResponse;
+use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
+use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 final class ChainProcessor implements InputProcessor, OutputProcessor
@@ -18,9 +24,14 @@ final class ChainProcessor implements InputProcessor, OutputProcessor
     private string $outputStructure;
 
     public function __construct(
-        private readonly ResponseFormatFactoryInterface $responseFormatFactory,
-        private readonly SerializerInterface $serializer,
+        private readonly ResponseFormatFactoryInterface $responseFormatFactory = new ResponseFormatFactory(),
+        private ?SerializerInterface $serializer = null,
     ) {
+        if (null === $this->serializer) {
+            $propertyInfo = new PropertyInfoExtractor([], [new PhpDocExtractor()]);
+            $normalizers = [new ObjectNormalizer(propertyTypeExtractor: $propertyInfo), new ArrayDenormalizer()];
+            $this->serializer = $serializer ?? new Serializer($normalizers, [new JsonEncoder()]);
+        }
     }
 
     public function processInput(Input $input): void
