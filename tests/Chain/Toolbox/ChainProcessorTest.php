41a01a3efd90baeda87a00b6308ec0a37ccc3aad
Philip Heimböck <philip.heimboeck@abacus.ch>
2025-05-23 11:17:31 +0200

    feat: add option to keep tool messages (#321)
    Fixes #321

--- tests/Chain/Toolbox/ChainProcessorTest.php
+++ tests/Chain/Toolbox/ChainProcessorTest.php
@@ -4,18 +4,26 @@ declare(strict_types=1);
 
 namespace PhpLlm\LlmChain\Tests\Chain\Toolbox;
 
+use PhpLlm\LlmChain\Chain;
 use PhpLlm\LlmChain\Chain\Input;
+use PhpLlm\LlmChain\Chain\Output;
 use PhpLlm\LlmChain\Chain\Toolbox\ChainProcessor;
 use PhpLlm\LlmChain\Chain\Toolbox\ExecutionReference;
 use PhpLlm\LlmChain\Chain\Toolbox\Metadata;
 use PhpLlm\LlmChain\Chain\Toolbox\ToolboxInterface;
 use PhpLlm\LlmChain\Exception\MissingModelSupport;
 use PhpLlm\LlmChain\Model\LanguageModel;
+use PhpLlm\LlmChain\Model\Message\AssistantMessage;
 use PhpLlm\LlmChain\Model\Message\MessageBag;
+use PhpLlm\LlmChain\Model\Message\ToolCallMessage;
+use PhpLlm\LlmChain\Model\Response\ToolCall;
+use PhpLlm\LlmChain\Model\Response\ToolCallResponse;
+use PhpLlm\LlmChain\PlatformInterface;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\UsesClass;
 use PHPUnit\Framework\TestCase;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 #[CoversClass(ChainProcessor::class)]
 #[UsesClass(Input::class)]
@@ -92,5 +100,61 @@ class ChainProcessorTest extends TestCase
         $input = new Input($llm, new MessageBag(), []);
 
         $chainProcessor->processInput($input);
+    }
+
+    #[Test]
+    public function processOutputWithToolCallResponseKeepingMessages(): void
+    {
+        $toolbox = $this->createMock(ToolboxInterface::class);
+        $toolbox->expects($this->once())->method('execute')->willReturn('Test response');
+
+        $llm = $this->createStub(LanguageModel::class);
+
+        $eventDispatcher = $this->createMock(EventDispatcherInterface::class);
+        $eventDispatcher->expects($this->once())->method('dispatch');
+
+        $messageBag = new MessageBag();
+
+        $response = new ToolCallResponse(new ToolCall('id1', 'tool1', ['arg1' => 'value1']));
+
+        $chain = new Chain($this->createStub(PlatformInterface::class), $llm);
+
+        $chainProcessor = new ChainProcessor($toolbox, eventDispatcher: $eventDispatcher, keepToolMessages: true);
+        $chainProcessor->setChain($chain);
+
+        $output = new Output($llm, $response, $messageBag, []);
+
+        $chainProcessor->processOutput($output);
+
+        self::assertCount(2, $messageBag);
+        self::assertInstanceOf(AssistantMessage::class, $messageBag->getMessages()[0]);
+        self::assertInstanceOf(ToolCallMessage::class, $messageBag->getMessages()[1]);
+    }
+
+    #[Test]
+    public function processOutputWithToolCallResponseForgettingMessages(): void
+    {
+        $toolbox = $this->createMock(ToolboxInterface::class);
+        $toolbox->expects($this->once())->method('execute')->willReturn('Test response');
+
+        $llm = $this->createStub(LanguageModel::class);
+
+        $eventDispatcher = $this->createMock(EventDispatcherInterface::class);
+        $eventDispatcher->expects($this->once())->method('dispatch');
+
+        $messageBag = new MessageBag();
+
+        $response = new ToolCallResponse(new ToolCall('id1', 'tool1', ['arg1' => 'value1']));
+
+        $chain = new Chain($this->createStub(PlatformInterface::class), $llm);
+
+        $chainProcessor = new ChainProcessor($toolbox, eventDispatcher: $eventDispatcher, keepToolMessages: false);
+        $chainProcessor->setChain($chain);
+
+        $output = new Output($llm, $response, $messageBag, []);
+
+        $chainProcessor->processOutput($output);
+
+        self::assertCount(0, $messageBag);
     }
 }
